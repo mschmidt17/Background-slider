127cc7e201c01d81283155fbaaaee815d8cd8928
Matias Schmidt <matias.schmidt@outlook.es>
2022-09-15 10:43:56 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import images from './img/images';
 import './App.css';
 
 function App() {
+  const [slide, setSlide] = useState(1);
+  const length = images.length;
+
+  const handlePrev = () => {
+    setSlide((slide) => (slide <= 1 ? length : slide - 1));
+  };
+
+  const handleNext = () => {
+    setSlide((slide) => (slide >= length ? 1 : slide + 1));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{ "backgroundImage": `url("${images[slide - 1].url}")` }}
+    >
+      <div className="slider-container">
+        {length > 0 &&
+          images.map((image) => (
+            <div
+              key={image.id}
+              className={`slide${slide === image.id ? ' active' : ''}`}
+              style={{
+                "backgroundImage": `url("${image.url}")`,
+              }}
+            ></div>
+          ))}
+        <button className="arrow left-arrow" onClick={handlePrev}>
+          <FontAwesomeIcon icon={faArrowLeft} />
+        </button>
+        <button className="arrow right-arrow" onClick={handleNext}>
+          <FontAwesomeIcon icon={faArrowRight} />
+        </button>
+      </div>
     </div>
   );
 }
